b4282b1a0eb030da5209a7e54d15ef4877174f86
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2019-02-18 14:39:31 +0100

    style(lint): lint code

--- Package.swift
+++ Package.swift
@@ -6,15 +6,16 @@ import PackageDescription
 let package = Package(
     name: "pomodoro-cli",
     dependencies: [
-      // Dependencies declare other packages that this package depends on.
-      // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "pomodoro-cli",
-            dependencies: ["Commander"]),
+            dependencies: ["Commander"]
+        ),
     ]
 )
