39db77939f418e063328bf8bfd3d49f811b96c83
Mick F <mick@mickf.net>
2020-10-22 11:51:54 +0200

    fix(deploy): use SwiftToolsSupport-auto product so that the code remains in 1 binary
    As explained here: https://developer.apple.com/forums/thread/655937

--- Package.swift
+++ Package.swift
@@ -16,7 +16,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Pomodoro", dependencies: ["ArgumentParser", "SwiftToolsSupport"]),
+        .target(name: "Pomodoro", dependencies: [
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core")
+        ]),
         .target(name: "PomodoroCLI", dependencies: ["Pomodoro"]),
         .testTarget(name: "PomodoroTests", dependencies: ["Pomodoro"]),
     ]
