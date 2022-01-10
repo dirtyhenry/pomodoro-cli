3be23099c2faac2b0a7cc16ae049116140068bef
Mickaël Floc’hlay <mickael.flochlay@memo.bank>
2022-01-10 16:59:31 +0100

    style: 💄 Apply linting

--- Sources/Pomodoro/TimeIntervalExtension.swift
+++ Sources/Pomodoro/TimeIntervalExtension.swift
@@ -5,7 +5,7 @@ enum TimeIntervalConversionError: Error {
     case unsupportedFormat
 }
 
-extension TimeInterval {
+public extension TimeInterval {
     // MARK: - Creating a time internal from a human readable expression.
 
     /// Interprets a duration from a human readable string.
@@ -14,7 +14,7 @@ extension TimeInterval {
     ///
     /// * A string **with digits only** will be parsed as a number of **seconds** (example: `123`);
     /// * A string **finishing with `m`** will be parsed as a number of **minutes** (example: `123m` or `123 m`).
-    public static func fromHumanReadableString(_ string: String) throws -> TimeInterval {
+    static func fromHumanReadableString(_ string: String) throws -> TimeInterval {
         let normalizedInput = string.replacingOccurrences(of: " ", with: "")
 
         guard let lastChar = normalizedInput.last else {
