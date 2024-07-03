18bd1bd0d9bb88f82d1b51a0dc5cd1ff81a9593e
Mick F <mick@mickf.net>
2024-07-03 20:56:53 +0200

    build: add Blocks deps

--- Package.swift
+++ Package.swift
@@ -5,16 +5,24 @@ import PackageDescription
 let package = Package(
     name: "Pomodoro",
     platforms: [
-        .macOS(.v10_13),
+        .macOS(.v10_15),
     ],
     products: [
         .executable(name: "pomodoro-cli", targets: ["PomodoroCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4")
+        .package(
+            url: "https://github.com/dirtyhenry/swift-blocks",
+            from: "0.4.0"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            from: "1.1.4"
+        )
     ],
     targets: [
         .target(name: "Pomodoro", dependencies: [
+            .product(name: "Blocks", package: "swift-blocks"),
             .product(name: "ArgumentParser", package: "swift-argument-parser")
         ]),
         .executableTarget(name: "PomodoroCLI", dependencies: ["Pomodoro"]),
